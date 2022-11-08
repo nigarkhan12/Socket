e60002f3db3439f3a080128559355c5fedfe5268
Nigar Khan <nigarkhan92@gmail.com>
2022-11-09 00:04:10 +0530

    modified index.html

--- Socket-2/index.js
+++ Socket-2/index.js
@@ -8,6 +8,10 @@ const expressServer = http.createServer(app);
 const {Server}=require('socket.io');
 const io = new Server(expressServer);
 
+io.on('connection', function(socket){
+    console.log("New User Connected");
+})
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
