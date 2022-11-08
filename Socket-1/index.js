2d2387fb6de39fdbd94b5c54152c944d4c1de561
Nigar Khan <nigarkhan92@gmail.com>
2022-11-08 13:09:16 +0530

    connection & disconnect event added

--- Socket-1/index.js
+++ Socket-1/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const { disconnect } = require('process');
 
 const expressServer = http.createServer(app);
 
 const {Server}=require('socket.io');
-const io = new Server(expressServer)
+const io = new Server(expressServer);
+
+io.on('connection', function(socket){
+    console.log("New user connected");
+
+    socket.on('disconnect', function(){
+        console.log("User disconnected");
+    })
+})
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
